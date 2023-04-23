2a0452423e7c3d19558091b2cb2a182e3f30b9fa
tomas <tomas-wsl2@Tomula.localdomain>
2023-04-23 17:26:44 +0200

    Refactor SpotifyClient

--- src/web/spotify_client/__init__.py
+++ src/web/spotify_client/__init__.py
@@ -6,15 +6,15 @@ class SpotifyClient:
     def __init__(self, access_token: str):
         self.access_token = access_token
 
-    def _send_request(self, url: str):
+    def _send_request(self, url: str) -> dict:
         headers = {"Authorization": "Bearer " + self.access_token}
         response = requests.get(url, headers=headers)
-
-        return response
-
-    def get_user_profile(self) -> dict:
-        endpoint = self.api_url + "me"
-        response = self._send_request(endpoint)
         response_data = response.json()
 
         return response_data
+
+    def get_user_profile(self) -> dict:
+        endpoint = self.api_url + "me"
+        data = self._send_request(endpoint)
+
+        return data
