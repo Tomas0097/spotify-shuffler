ccd178098fc9877de856428508f4b1e25605c349
tomas <tomas-wsl2@Tomula.localdomain>
2023-05-28 22:04:42 +0200

    Refactor ProfileView

--- src/web/views/main.py
+++ src/web/views/main.py
@@ -13,10 +13,8 @@ class ProfileView(SpotifyErrorHandlerMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         user_access_token = self.request.session.get("user_access_token", "")
-        profile_data = SpotifyClient(
-            user_access_token=user_access_token
-        ).get_user_profile_data()
-
+        spotify_client = SpotifyClient(user_access_token=user_access_token)
+        profile_data = spotify_client.get_user_profile_data()
         context_data = super().get_context_data(**kwargs)
         context_data.update({"profile_name": profile_data["display_name"]})
 
