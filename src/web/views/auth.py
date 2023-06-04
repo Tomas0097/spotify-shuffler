7e8d435408633efb9829d46610232d1a98363655
Tomas Rymon <tomas.rymon@gmail.com>
2023-06-04 18:22:14 +0200

    Refactor auth views

--- src/web/views/auth.py
+++ src/web/views/auth.py
@@ -2,34 +2,33 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import View
 
-from web.spotify_client import SpotifyClient
+from web.spotify_client.exceptions import SpotifyAPIError
+from web.views.main import SpotifyClientView
 
 
-class SpotifyLinkAccountView(View):
-    def get(self, request):
+class SpotifyLinkAccountView(SpotifyClientView):
+    def get(self, request, *args, **kwargs):
         authorization_code = request.GET.get("code")
         error = request.GET.get("error")
 
+        # Spotify API Authorization process sends an authorization
+        # code if the user has login successfully.
         if authorization_code:
-            user_access_token = SpotifyClient().get_user_access_token(
+            user_access_token = self.spotify_client.get_user_access_token(
                 authorization_code
             )
             request.session["user_access_token"] = user_access_token
-
             return HttpResponseRedirect(reverse("web:profile"))
 
-        # TODO: Handle this error.
         elif error:
-            return HttpResponse()
+            raise SpotifyAPIError
 
         else:
-            spotify_authorization_url = SpotifyClient().get_authorization_url()
-
+            spotify_authorization_url = self.spotify_client.get_authorization_url()
             return HttpResponseRedirect(spotify_authorization_url)
 
 
 class SpotifyLogoutView(View):
     def get(self, request):
         del request.session["user_access_token"]
-
         return HttpResponseRedirect(reverse("web:homepage"))
